f5b935059b097850a6edf5e658613e236e3720a1
Abhay2004Kumar <abs220502@gmail.com>
2025-04-19 16:50:24 +0530

    updating some changes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import { useNavigate } from 'react-router-dom'; 
 import Navbar from '../components/home/Navbar';
 import Header from '../components/contact/Header';
 import Border from '../components/home/Border';
-
+import SignupPopup from '../components/home/SignupPopup';
 
 const Contact = () => {
+  const [showPopup, setShowPopup] = useState(false);
   return (
     <div>
-      <Navbar/>
+       <Navbar onSignUpClick={() => setShowPopup(true)} />
       <Header/>
       <Border/>
+      {showPopup && <SignupPopup onClose={() => setShowPopup(false)} />}
     </div>
   );
 };
