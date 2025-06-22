2d3a9273ea0a5cb2c6f3719464363bd441ae0450
The-YashGaur <gauryash0904@gmail.com>
2025-06-22 22:13:36 +0530

    commit added

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -10,10 +10,10 @@ const Hero = () => {
       >
         <div className="flex flex-col justify-center items-start gap-6 sm:gap-8 md:gap-10 w-full md:w-[80%] lg:w-[60%]">
           <div className="flex flex-col justify-center items-start space-y-2">
-            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-6xl font-semibold">
+            <h1 className="text-2xl  font-bold sm:text-3xl md:text-4xl lg:text-6xl ">
               Holistic Healthcare
             </h1>
-            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-6xl font-semibold">
+            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-6xl font-bold">
               Experience
             </h1>
           </div>
